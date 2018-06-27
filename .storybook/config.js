5db2c1781778db50c00ce52d705e544b48d255b0
Mari NAKANO <nakano@val.co.jp>
2018-06-27 16:03:16 +0900

    Storybookのオプションを追加し、開いた時にKnobsを開くように指定しました。

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,16 @@
 import { configure } from '@storybook/vue';
+import { setOptions } from '@storybook/addon-options';
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../src/stories', true, /.stories.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
+// Storybookのオプションを定義する。
+setOptions({
+  // 最初にknobsを開くように指定。
+  selectedAddonPanel: 'knobs',
+});
 
 configure(loadStories, module);
